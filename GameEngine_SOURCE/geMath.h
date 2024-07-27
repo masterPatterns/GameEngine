71f549b8a61210548a83dee654e6ce7642367958
sacur <sacur@REMINGRO>
2024-07-27 14:06:21 +0900

    math 함수 추가(Dot, Cross, Rotate)

--- GameEngine_SOURCE/geMath.h
+++ GameEngine_SOURCE/geMath.h
@@ -3,6 +3,7 @@
 
 namespace ge::math
 {
+#define PI 3.141592f
 	struct Vector2
 	{
 		static Vector2 One;
@@ -39,5 +40,46 @@ namespace ge::math
 		{
 			return Vector2(x / value, y / value);
 		}
+
+		void clear()
+		{
+			x = 0.0f;
+			y = 0.0f;
+		}
+
+		float length()
+		{
+			return sqrtf(x * x + y * y);
+		}
+
+		Vector2 normalize()
+		{
+			float len = length();
+			x /= len;
+			y /= len;
+
+			return *this;
+		}
+
 	};
+
+	Vector2 Rotate(Vector2 vector, float degree)
+	{
+		float radian = (degree / 180.0f) * PI;
+		vector.normalize();
+		float x = cosf(radian) * vector.x - sinf(radian) * vector.y;
+		float y = sinf(radian) * vector.x + cosf(radian) * vector.y;
+
+		return Vector2(x, y);
+	}
+
+	float Dot(Vector2& v1, Vector2 v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+
+	float Cross(Vector2 v1, Vector2 v2)
+	{
+		return v1.x * v2.y - v1.y * v2.x;
+	}
 }
